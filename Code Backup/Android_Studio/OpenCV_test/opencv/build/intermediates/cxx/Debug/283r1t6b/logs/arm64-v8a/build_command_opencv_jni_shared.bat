81de1bb8fef54c87eae82e9a8cada9037b12ac15
bhamicus42 <archetypetheta@gmail.com>
2023-05-01 10:59:22 -0700

    copy open_CV, tweak MiDAS
    Copied open_CV to the DE directory for experimentation (I realized after the fact that this was unnecessary)
    
    made some small changes to MiDAS, but I'm not sure what changed relative to last commit.

--- Code Backup/Android_Studio/OpenCV_test/opencv/build/intermediates/cxx/Debug/283r1t6b/logs/arm64-v8a/build_command_opencv_jni_shared.bat
+++ Code Backup/Android_Studio/OpenCV_test/opencv/build/intermediates/cxx/Debug/283r1t6b/logs/arm64-v8a/build_command_opencv_jni_shared.bat
@@ -1,5 +1,5 @@
 @echo off
-"C:\\Users\\miker\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+"C:\\Users\\arche\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
   -C ^
-  "C:\\Users\\miker\\AndroidStudioProjects\\OpenCV_test\\opencv\\.cxx\\Debug\\283r1t6b\\arm64-v8a" ^
+  "C:\\Users\\arche\\Documents\\GitHub\\ECE191-SmartGlasses\\Code Backup\\Android_Studio\\OpenCV_test\\opencv\\.cxx\\Debug\\283r1t6b\\arm64-v8a" ^
   opencv_jni_shared
---
+++ Code Backup/DE/OpenCV_test/opencv/build/intermediates/cxx/Debug/283r1t6b/logs/arm64-v8a/build_command_opencv_jni_shared.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\arche\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\Users\\arche\\Documents\\GitHub\\ECE191-SmartGlasses\\Code Backup\\DE\\OpenCV_test\\opencv\\.cxx\\Debug\\283r1t6b\\arm64-v8a" ^
+  opencv_jni_shared
