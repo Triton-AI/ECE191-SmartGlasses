30ff6918f727bceca475cdd926a4f97f5be1a46e
bhamicus42 <archetypetheta@gmail.com>
2023-05-17 12:27:32 -0700

    undoing gauss filter
    last commit removed files that were needed for the Android Studio project

--- Code Backup/DE/OpenCV_test/opencv/build/intermediates/cxx/Debug/283r1t6b/logs/x86_64/build_command_opencv_jni_shared.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\arche\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\Users\\arche\\Documents\\GitHub\\ECE191-SmartGlasses\\Code Backup\\Android_Studio\\OpenCV_test\\opencv\\.cxx\\Debug\\283r1t6b\\x86_64" ^
-  opencv_jni_shared
